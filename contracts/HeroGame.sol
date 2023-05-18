0d6332bf3bfa35c5776c165657688126979c44dd
tom yuan <t1123425@gmail.com>
2023-05-18 17:21:34 +0800

    add base64 libraries & sample contract & script & update game contract & script

--- contracts/HeroGame.sol
+++ contracts/HeroGame.sol
@@ -8,12 +8,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // Helper functions OpenZeppelin provides.
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-
+// Helper we wrote to encode in Base64
+import "./libraries/Base64.sol";
 import "hardhat/console.sol";
 
-contract HeroGame is ERC721 {
-    // We'll hold our character's attributes in a struct. Feel free to add
-    // whatever you'd like as an attribute! (ex. defense, crit chance, etc).
+contract HeroGame is ERC721{
+    // 下列struct為腳色屬性，包含腳色id,名稱,圖片連結,生命值(HP),生命值上限(maxHp),攻擊力(attackDamage)
     struct CharacterAttributes {
         uint characterIndex;
         string name;
@@ -22,79 +22,80 @@ contract HeroGame is ERC721 {
         uint maxHp;
         uint attackDamage;
     }
+    // The tokenId is the NFTs unique identifier, it's just a number that goes
+    //產生每個nft的獨立TokenID，這邊使用openzeppelin的counter函式，當每次呼叫就會+1
+    // 0, 1, 2, 3, etc.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  // The tokenId is the NFTs unique identifier, it's just a number that goes
-  // 0, 1, 2, 3, etc.
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+    // We create a mapping from the nft's tokenId => that NFTs attributes.
+    //這裡創建mapping來映射對應ID獲得合約內記錄創建的nft
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
 
-  CharacterAttributes[] defaultCharacters;
-
-  // We create a mapping from the nft's tokenId => that NFTs attributes.
-  mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
-
-  // A mapping from an address => the NFTs tokenId. Gives me an ez way
-  // to store the owner of the NFT and reference it later.
-  mapping(address => uint256) public nftHolders;
-
-  constructor(
-    string[] memory characterNames,
-    string[] memory characterImageURIs,
-    uint[] memory characterHp,
-    uint[] memory characterAttackDmg
-    // Below, you can also see I added some special identifier symbols for our NFT.
-    // This is the name and symbol for our token, ex Ethereum and ETH. I just call mine
-    // Heroes and HERO. Remember, an NFT is just a token!
-  )
-    ERC721("Heroes", "HERO")
-  {
-    for(uint i = 0; i < characterNames.length; i += 1) {
-      defaultCharacters.push(CharacterAttributes({
-        characterIndex: i,
-        name: characterNames[i],
-        imageURI: characterImageURIs[i],
-        hp: characterHp[i],
-        maxHp: characterHp[i],
-        attackDamage: characterAttackDmg[i]
-      }));
-
-      CharacterAttributes memory c = defaultCharacters[i];
-      
-      // Hardhat's use of console.log() allows up to 4 parameters in any order of following types: uint, string, bool, address
-      console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+    // A mapping from an address => the NFTs tokenId. Gives me an ez way
+    // to store the owner of the NFT and reference it later.
+    //透過mapping映射從使用者的錢包地址來獲取nft tokenId，
+    //並且將該nft的持有者儲存成nftHolders
+    mapping(address => uint256) public nftHolders;
+    constructor()ERC721("O_WorldHeroes", "HERO"){
+        console.log("I deploy O_world heros smart contract");
+        // 當創建合約時這邊_tokenIds將會+1(預設counter會從0開始，而0在solidity為默認值)
+        // 所以將下來創建NFT的第一支教穡將會從1開始
+        _tokenIds.increment();
     }
+    //創建NFT腳色，會需要入腳色NFT 名稱,圖片連結,(生命值/生命值上限),攻擊力
+    function mintCharacterNFT(
+        string memory Name,
+        string memory ImageURI,
+        uint Hp,
+        uint AttackDamage) external{
 
-    // I increment _tokenIds here so that my first NFT has an ID of 1.
-    // More on this in the lesson!
-    _tokenIds.increment();
-  }
+        // 獲取當前_tokenId (初始會從1開始)
+        uint256 newItemId = _tokenIds.current();
+        // The magical function! Assigns the tokenId to the caller's wallet address.
+        //根據newItemId 鑄造 NFT
+        _safeMint(msg.sender, newItemId);
 
-  // Users would be able to hit this function and get their NFT based on the
-  // characterId they send in!
-  function mintCharacterNFT(uint _characterIndex) external {
-    // Get current tokenId (starts at 1 since we incremented in the constructor).
-    uint256 newItemId = _tokenIds.current();
+        //添加進遊戲腳色進入nftHolderAttributes的映射變量
+         nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex:newItemId,
+            name: Name,
+            imageURI: ImageURI,
+            hp: Hp,
+            maxHp: Hp,
+            attackDamage: AttackDamage
+        });
+         // Keep an easy way to see who owns what NFT.
+        nftHolders[msg.sender] = newItemId;
 
-    // The magical function! Assigns the tokenId to the caller's wallet address.
-    _safeMint(msg.sender, newItemId);
+        // Increment the tokenId for the next person that uses it.
+        _tokenIds.increment();
 
-    // We map the tokenId => their character attributes. More on this in
-    // the lesson below.
-    nftHolderAttributes[newItemId] = CharacterAttributes({
-      characterIndex: _characterIndex,
-      name: defaultCharacters[_characterIndex].name,
-      imageURI: defaultCharacters[_characterIndex].imageURI,
-      hp: defaultCharacters[_characterIndex].hp,
-      maxHp: defaultCharacters[_characterIndex].maxHp,
-      attackDamage: defaultCharacters[_characterIndex].attackDamage
-    });
+    }
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+      CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
 
-    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
-    
-    // Keep an easy way to see who owns what NFT.
-    nftHolders[msg.sender] = newItemId;
+      string memory strHp = Strings.toString(charAttributes.hp);
+      string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+      string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
 
-    // Increment the tokenId for the next person that uses it.
-    _tokenIds.increment();
+      string memory json = Base64.encode(
+        abi.encodePacked(
+          '{"name": "',
+          charAttributes.name,
+          ' -- NFT #: ',
+          Strings.toString(_tokenId),
+          '", "description": "My name is ',charAttributes.name,' Im a Hero of the O-world !", "image": "',
+          charAttributes.imageURI,
+          '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+          strAttackDamage,'} ]}'
+        )
+      );
+
+      string memory output = string(
+        abi.encodePacked("data:application/json;base64,", json)
+      );
+      
+      return output;
   }
 }
--- scripts/run_sample.js
+++ scripts/run_sample.js
@@ -1,12 +1,18 @@
 const main = async () => {
-    const gameContractFactory = await hre.ethers.getContractFactory('HeroGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContractFactory = await hre.ethers.getContractFactory('HeroGame_sample');
+    const gameContract = await gameContractFactory.deploy(
+      ["Iron man", "Spider Women", "Takeshi Kaneshiro"],       // Names
+      ["https://i.imgur.com/8KfVht6.jpeg", // Images
+      "https://i.imgur.com/rIMOnWm.jpeg", 
+      "https://i.imgur.com/0dzpfWQ.jpeg"],
+      [100, 200, 300],                    // HP values
+      [100, 50, 25]
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
     // We only have three characters.
     // an NFT w/ the character at index of our array.
-    //mintCharacterNFT param:名稱,圖片連結,生命值,生命值上限,攻擊力
-    let txn = await gameContract.mintCharacterNFT('金城武','https://i.imgur.com/0dzpfWQ.jpeg','200','300');
+    let txn = await gameContract.mintCharacterNFT(0);
     await txn.wait();
 
     // Get the value of the NFT's URI.
@@ -24,4 +30,4 @@ const runMain = async () => {
     }
   };
   
-  runMain()
+  runMain();
